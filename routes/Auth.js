f72eae007900cc5ff0bde93c833ccdc0083c7359
mixro <100027165+mixro@users.noreply.github.com>
2023-08-21 01:46:33 +0300

    Update Auth.js

--- routes/Auth.js
+++ routes/Auth.js
@@ -40,7 +40,7 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.body.email });
+        const user = await User.findOne({ email: req.body.email });
 
         const hashedPassword = CryptoJS.AES.decrypt(
             user.password,
