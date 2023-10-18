ac1416c9a18e361254ac0d65e80877beb38f51a3
i CEP <josephchongola43@gmail.com>
2023-10-18 17:34:53 +0300

    modified:   routes/Auth.js

--- routes/Auth.js
+++ routes/Auth.js
@@ -88,50 +88,10 @@ router.post("/login", async (req, res) => {
 });
   
 
-//GOOGLE AUTH
-router.post("/google", async (req, res) => {
+//GOOGLE LOGIN
+router.post("/google-login", async (req, res) => {
     try {
       const user = await User.findOne({ email: req.body.email });
-  
-      if (!user) {
-        const temporaryPassword = Math.random().toString(36).substring(7);
-  
-        const newUser = new User({
-          username: req.body.username,
-          email: req.body.email,
-          phoneNumber: req.body.phoneNumber,
-          img: req.body.img,
-          gender: req.body.gender,
-          course: req.body.course,
-          password: CryptoJS.AES.encrypt(
-            temporaryPassword,
-            process.env.PASS_SEC
-          ).toString(),
-        });
-  
-        const savedUser = await newUser.save();
-  
-        // After saving the new user, you can find their associated projects here
-        const projects = await Project.find({ userId: savedUser._id });
-  
-        // Add the projects array to the saved user
-        savedUser.projectsArray = projects;
-  
-        await savedUser.save();
-  
-        const accessToken = jwt.sign(
-          {
-            id: savedUser._id,
-            isAdmin: savedUser.isAdmin,
-          },
-          process.env.JWT_SEC,
-          { expiresIn: "86400d" }
-        );
-  
-        const { password, ...others } = savedUser._doc;
-  
-        res.status(201).json({ ...others, accessToken });
-      }
   
       const accessToken = jwt.sign(
         {
@@ -150,6 +110,51 @@ router.post("/google", async (req, res) => {
       res.status(200).json({ ...others, accessToken, projects });
     } catch (error) {
       return res.status(500).json("Error while logging in");
+    }
+});
+
+//GOOGLE REGISTER
+router.post("/google-register", async (req, res) => {
+    try {
+      const temporaryPassword = Math.random().toString(36).substring(7);
+
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        img: req.body.img,
+        gender: req.body.gender,
+        course: req.body.course,
+        password: CryptoJS.AES.encrypt(
+          temporaryPassword,
+          process.env.PASS_SEC
+        ).toString(),
+      });
+
+      const savedUser = await newUser.save();
+
+      // After saving the new user, you can find their associated projects here
+      const projects = await Project.find({ userId: savedUser._id });
+
+      // Add the projects array to the saved user
+      savedUser.projectsArray = projects;
+
+      await savedUser.save();
+
+      const accessToken = jwt.sign(
+        {
+          id: savedUser._id,
+          isAdmin: savedUser.isAdmin,
+        },
+        process.env.JWT_SEC,
+        { expiresIn: "86400d" }
+      );
+
+      const { password, ...others } = savedUser._doc;
+
+      res.status(201).json({ ...others, accessToken });
+    } catch (error) {
+      return res.status(500).json("Error while registering");
     }
 });
   
